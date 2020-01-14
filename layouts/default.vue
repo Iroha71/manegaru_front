2179ae8a60d0bb64d77e5f5a676b746a9beefd8f
Iroha71 <y.koba.h1071@outlook.jp>
2020-01-14 22:26:08 +0900

    ➕基本レイアウト

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="root">
+    <b-navbar type="is-primary"></b-navbar>
+    <div class="hero is-fullheight-with-navbar">
+      <div class="columns">
+        <div class="column is-2 has-background-grey-light">
+          <aside class="menu">
+            <p class="menu-label">メニュー</p>
+            <ul><li>リスト</li></ul>
+          </aside>
+        </div>
+        <section class="column">
+          <nuxt />
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.columns {
+  margin: 0;
+  height: calc(100vh - 3.25rem);
+  section.column {
+    overflow-y: scroll;
+  }
+}
+</style>
