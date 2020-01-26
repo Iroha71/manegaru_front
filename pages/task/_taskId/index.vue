287c4985e8d5de330d1039819c7eb63fa866001c
Iroha71 <y.koba.h1071@outlook.jp>
2020-01-26 18:48:31 +0900

    🔼タスク状態変更機能追加 #6

--- pages/task/_taskId/index.vue
+++ pages/task/_taskId/index.vue
@@ -2,12 +2,12 @@
 <div class="section column is-12-tablet">
     <div class="content">
         <h1 :class="getStatusColor()">{{ task.title }}</h1>
-        <b-tag size="is-medium">Lv.{{ task.priority.like_rate }}</b-tag>
+        <b-tag size="is-medium" :type="getPriorityColor()">Lv.{{ task.priority.like_rate }}</b-tag>
         <b-tag size="is-medium">{{ task.status }}</b-tag>
     </div>
     <div class="columns">
         <div class="column is-4 has-text-centered">
-            <b-steps class="has-text-centered" v-model="statusIndex" size="is-medium" type="is-danger" :has-navigation="false">
+            <b-steps class="has-text-centered is-hidden-touch" v-model="statusIndex" size="is-medium" type="is-danger" :has-navigation="false">
                 <b-step-item clickable type="is-danger">
                     <h2 class="title has-text-centered has-text-danger">未着手</h2>
                 </b-step-item>
@@ -18,15 +18,26 @@
                     <h2 class="title has-text-centered has-text-success">完了</h2>
                 </b-step-item>
             </b-steps>
-            <b-button type="is-success" v-if="isStatusUpdated">更新</b-button>
+            <b-steps class="has-text-centered is-hidden-desktop" v-model="statusIndex" size="is-medium" type="is-danger">
+                <b-step-item clickable type="is-danger">
+                    <h2 class="title has-text-centered has-text-danger">未着手</h2>
+                </b-step-item>
+                <b-step-item clickable type="is-info">
+                    <h2 class="title has-text-centered has-text-info">作業中</h2>
+                </b-step-item>
+                <b-step-item clickable type="is-success">
+                    <h2 class="title has-text-centered has-text-success">完了</h2>
+                </b-step-item>
+            </b-steps>
+            <b-button type="is-success" v-if="isStatusUpdated" @click="changeStatus()">更新</b-button>
         </div>
         <div class="column content">
             <table >
                 <tr class="has-background-grey-lighter">
                     <th class="has-text-centered">期限</th>
-                    <td >{{ task.limit_date }}</td>
+                    <td :class="`has-background-${getStatusColor()}`">{{ task.limit_date }}</td>
                 </tr>
-                <tr class="has-background-info">
+                <tr class="has-background-grey-light">
                     <td colspan="2" class="has-text-centered">
                         {{ task.updated_at }}
                         <b-tag type="is-info" size="is-medium" v-if="task.is_updated">更新</b-tag>
@@ -38,7 +49,7 @@
     </div>
     <hr>
     <div class="content">
-        <b-tag type="is-info" size="is-large">メモ</b-tag>
+        <b-tag :type="`is-${getStatusColor()}`" size="is-large">メモ</b-tag>
         <div class="memo-area">
             <span v-if="task.detail">{{ task.detail }}</span>
             <span v-else>メモは書かれていません</span>
@@ -48,6 +59,7 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
     async asyncData({store, route}) {
         const taskId = route.params.taskId
@@ -61,21 +73,40 @@ export default {
             statuses: ['未着手', '作業中', '完了']
         }
     },
-    mounted() {
-        this.defaultIndex = this.statuses.indexOf(this.task.status)
-        this.statusIndex = this.defautIndex
+    created() {
+        this.statusIndex = this.statuses.indexOf(this.task.status)
+        this.defautIndex = this.statusIndex
     },
     methods: {
+        ...mapActions({ 'updateStatus': 'task/updateStatus' }),
         getStatusColor:function() {
             switch(this.task.status) {
                 case this.statuses[0]:
-                    return 'yet'
+                    return 'danger'
                 case this.statuses[1]:
-                    return 'working'
+                    return 'info'
                 case this.statuses[2]:
-                    return 'done'
+                    return 'success'
                 default:
                     return ''
+            }
+        },
+        changeStatus:function() {
+            this.updateStatus({taskId: this.task.id, status: this.statuses[this.statusIndex]})
+            .then(updatedStatus => {
+                this.task.status = updatedStatus
+                this.defautIndex = this.statuses.indexOf(updatedStatus)
+                this.statusIndex = this.defautIndex
+            })
+        },
+        getPriorityColor:function() {
+            switch(this.task.priority.like_rate) {
+                case 1:
+                    return 'is-success'
+                case 2:
+                    return 'is-info'
+                case 3:
+                    return 'is-primary'
             }
         }
     },
@@ -96,13 +127,13 @@ h1 {
     border-left: solid 5px;
     border-bottom: solid 1px;
     padding-left: 5px;
-    &.yet {
+    &.danger {
         border-color: #ff3860;
     }
-    &.working {
+    &.info {
         border-color: #167df0;
     }
-    &.done {
+    &.success {
         border-color: #23d160;
     }
 }
@@ -122,5 +153,7 @@ table {
 }
 .memo-area {
     padding: 0.75rem;
+    white-space: pre-wrap;
+    word-wrap: break-word;
 }
 </style>
--- store/task.js
+++ store/task.js
@@ -7,5 +7,11 @@ export const actions = {
     async show({dispatch}, taskId) {
         const task = await dispatch('api/request', {method: 'get', endpoint: `task/${taskId}`, params: null}, {root: true})
         return task
+    },
+
+    async updateStatus({dispatch}, {taskId, status}) {
+        const statusInfo = { status: status }
+        const updatedStatus = await dispatch('api/request', {method: 'put', endpoint: `task/${taskId}/update_status`, params: statusInfo}, {root: true})
+        return updatedStatus.data
     }
 }
