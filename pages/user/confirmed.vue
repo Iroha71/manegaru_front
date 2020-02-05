1eee0985069df2da682dafba4ff1c38a67367f47
Iroha71 <y.koba.h1071@outlook.jp>
2020-02-06 00:29:36 +0900

    🔼認証済みメアドの際、エラーメッセージを表示 #11

--- pages/user/confirmed.vue
+++ pages/user/confirmed.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="section content has-text-centered">
+    <div class="section content has-text-centered" v-if="!$route.query.error">
         <div class="has-background-success icon-area"><img src="/icons/checkbox.png" class="has-background-success"></div>
         <h3>アカウントの認証が完了しました</h3>
         <h4>パスワードを入力して秘書を選びに行きましょう</h4>
         <validation-observer v-slot="{ invalid }" tag="div" class="column is-3">
             <Vinput type="text" label="メールアドレス" rules="required" v-model="email" />
             <Vinput type="password" label="パスワード" rules="required" v-model="password" />
-            <b-button size="is-large" type="is-success" :disabled="invalid" @click="goToGirlPage()">秘書選択へ</b-button>
+            <b-button size="is-large" type="is-success" :disabled="invalid" @click="goToGirlPage()">秘書を選択する</b-button>
         </validation-observer>
+    </div>
+    <div class="section content has-text-centered" v-else>
+        <h3>このメールアドレスは既に認証されています</h3>
+        <b-button type="is-success" size="is-large" @click="$router.push('/login')">サインインする</b-button>
     </div>
 </template>
 
