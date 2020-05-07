d31f8dc33d02e8c94ea3c56599f1bc761f3423e7
Iroha71 <y.koba.h1071@outlook.jp>
2020-05-07 12:18:43 +0900

    ♻不要なtry-catchを削除

--- plugins/axios.js
+++ plugins/axios.js
@@ -9,27 +9,22 @@ export default ({store, redirect, $axios, route}) => {
     $axios.onResponse(response => {
         setTimeout(() => {
             store.dispatch('api/stopLoad')
-        }, 800)
+        }, 300)
     })
 
     $axios.onError(error => {
         store.dispatch('api/stopLoad')
         console.log(error.response)
-        try{
-            if(error.response.status == 401 || error.response.status == 403) {
-                redirect(`/login/?error=${error.response.status}`)
-            } else if(error.response.status == 422) {
-                redirect(`${route.path}/?error=422`)
-            } else {
-                $nuxt.error({
-                    statusCode: error.response.status,
-                    message: 'サーバでエラーが発生しました'
-                })
-            }
-        }catch(catchError){
+        if(error.response.status == 401 || error.response.status == 403) {
+            redirect(301, `/login/?error=${error.response.status}`)
+            return
+        } else if(error.response.status == 422) {
+            redirect(`${route.path}?invalidError=${error.response.data.message}`)
+            return
+        } else {
             $nuxt.error({
-                statusCode: 500,
-                message: 'ネットワークエラーが発生しました' + error
+                statusCode: error.response.status,
+                message: 'サーバでエラーが発生しました'
             })
         }
     })
